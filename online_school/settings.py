e56f2199d166d50b8d63e98d7de384b6eaee0f33
umarfaruk2 <farukdev22@gmail.com>
2023-09-03 00:01:00 +0600

    update setting

--- online_school/settings.py
+++ online_school/settings.py
@@ -75,7 +75,7 @@ ROOT_URLCONF = 'online_school.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
