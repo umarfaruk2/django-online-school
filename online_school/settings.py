0a2abcccc6b52ecf7b0fe9d7d2e1f3bac9d803bd
umarfaruk2 <farukdev22@gmail.com>
2023-09-03 12:08:14 +0600

    update tailwind setting

--- online_school/settings.py
+++ online_school/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tailwind',
-    'theme',
-    'django_browser_reload',
+
     "crispy_forms",
     "crispy_tailwind",
     
